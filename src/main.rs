2a2d941c399ae17d66b677e122c344425fa892e2
niuhaihang <haihangniu@gmail.com>
2024-01-09 00:11:56 +0800

    MOD:hello

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use core::time;
 use std::error::Error; // 使用 use 引入一个标准库的包，或者第三方的包
 
-use axum::{routing::get,routing::post, Router};
+use axum::{routing::get, routing::post, Router};
 use std::net::SocketAddr;
 
 /// Rust 程序入口
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(test))
-    .route("/robot", post(send_msg));
+    let app = Router::new()
+        .route("/", get(test))
+        .route("/robot", get(send_msg));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
@@ -54,63 +55,77 @@ const WEBHOOK_URL: &str =
 const WEBHOOK_SECRET: &str = "TgliN11mPBnZCZIQuKg39";
 use chrono::prelude::*;
 use ring::{hmac, signature};
+use base64::encode;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::str;
 use std::string::FromUtf8Error;
-use serde::{Serialize, Deserialize};
-fn generate_signature(t:i64) -> Result<String, FromUtf8Error> {
-    let mut str_to_sign = t.to_string() + "\n" + WEBHOOK_SECRET;
-
+fn generate_signature(t: i64) -> Result<String, FromUtf8Error> {
+    let str_to_sign=format!("{}\n{}",t.to_string(),WEBHOOK_SECRET);
+    println!("{str_to_sign}");
     let key = hmac::Key::new(hmac::HMAC_SHA256, WEBHOOK_SECRET.as_bytes());
     let signature = hmac::sign(&key, str_to_sign.as_bytes());
-    String::from_utf8(signature.as_ref().to_vec())
+
+    Ok(encode(signature))
 }
-#[derive(Serialize,Deserialize)]
-struct MsgContent{
-    msg_type:String,
-    time_stamp:String,
-    sign:String,
-    content:String,
+#[derive(Serialize, Deserialize)]
+struct MsgContent {
+    msg_type: String,
+    time_stamp: String,
+    sign: String,
+    content: String,
 }
 
-async fn send_msg()->String{
-    let res = send_robot_msg().await?;
-    res
+async fn send_msg() -> String {
+    println!("send msg");
+    let res = send_robot_msg().await;
+    match res {
+        Ok(s) => s,
+        Err(e) => e.to_string(),
+    }
 }
 
-async fn send_robot_msg() -> Result<String,Box<dyn Error>> {
+async fn send_robot_msg() -> Result<String, Box<dyn Error>> {
     let now = Local::now();
     let time_stamp = now.timestamp();
     let signature = generate_signature(time_stamp)?;
 
-    let content = MsgContent{
-        msg_type:"text".to_string(),
-        time_stamp:time_stamp.to_string(),
-        sign:signature,
-        content:"hello".to_string(),
+    let content = MsgContent {
+        msg_type: "text".to_string(),
+        time_stamp: time_stamp.to_string(),
+        sign: signature,
+        content: r#"{"text":"hello"}"#.to_string(),
     };
+    // let mut map = HashMap::new();
+    // map.insert("msg_type", "text".to_string());
+    // map.insert("time_stamp", time_stamp.to_string());
+    // map.insert("sign", signature);
+    // map.insert("content", "hello".to_string());
 
     let cli = reqwest::Client::new();
 
     let content_str = serde_json::to_string(&content)?;
-    let resp = cli.post(WEBHOOK_URL)
-    .header("Content-Type",  "application/json")
-    .body(content_str)
-    .send()
-    .await?
-    .text()
-    .await?;
-    println!("resp is:{}",resp);
+    println!("msg is sending");
+    let resp = cli
+        .post(WEBHOOK_URL)
+        .header("Content-Type", "application/json")
+        .body(content_str)
+        .send()
+        .await?
+        .text()
+        .await?;
+    println!("resp is:{}", resp);
     Ok(resp)
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use std::thread;
 
     use super::*;
 
     #[test]
-    fn test_send_robot_msg(){
+    fn test_send_robot_msg() {
         let res = send_robot_msg();
         let ten_millis = time::Duration::from_millis(1000);
         thread::sleep(ten_millis);
